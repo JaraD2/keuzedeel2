aa863126e0cdbf97536d7178a4bb222be42f11f3
Jara Doornbos <81758642+JaraD2@users.noreply.github.com>
2024-05-22 15:30:04 +0200

    Add new files and directories

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,18 @@
 import {
-  Form,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import appStylesHref from "./app.css?url";
+import { LinksFunction } from "@remix-run/node";
+
+import Sidebar from "./components/sidebar";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
 
 export default function App() {
   return (
@@ -16,35 +24,10 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
+        <Sidebar />
+        <main>
+          <Outlet />
+        </main>
         <ScrollRestoration />
         <Scripts />
       </body>
